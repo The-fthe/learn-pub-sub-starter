81fa3ec14d2244b778b3a2c676d1b6deea03e2bd
fthe <lcslianandfthe.cs@gmail.com>
2024-07-06 18:00:52 +0900

    ch3:5 update

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	amqp, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+		return
+	}
+	defer amqp.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("get username failed", err)
+		return
+	}
+
+	exchange := routing.ExchangePerilDirect
+	queueName := routing.PauseKey + "." + username
+	routingKey := routing.PauseKey
+	pubsub.DeclareAndBind(amqp, exchange, queueName, routingKey, pubsub.SimpleQueueTransient)
+
+	//wait for ctrl +c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	amqp, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+		return
+	}
+	defer amqp.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	ch, err := amqp.Channel()
+	if err != nil {
+		log.Fatalf("channel err: %v", err)
+		return
+	}
+	exchange := routing.ExchangePerilDirect
+	key := routing.PauseKey
+	state := routing.PlayingState{
+		IsPaused: true,
+	}
+	err = pubsub.PublishJSON(ch, exchange, key, state)
+	if err != nil {
+		log.Fatalf("PublishErr: %v", err)
+		return
+	}
+	fmt.Println("Exchange channel created")
+
+	//wait for ctrl +c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
 }
